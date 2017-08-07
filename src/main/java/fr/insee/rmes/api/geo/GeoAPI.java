43cea9901689dace0d6f6242c99daafbe6046ac1
Franck Cotton <franck.cotton@insee.fr>
2017-08-07 10:42:40 +0200

    Added management of 404 responses in geo API

--- src/main/java/fr/insee/rmes/api/geo/GeoAPI.java
+++ src/main/java/fr/insee/rmes/api/geo/GeoAPI.java
@@ -5,6 +5,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,42 +22,45 @@ public class GeoAPI {
 	@Path("/commune/{code: [0-9][0-9AB][0-9]{3}}")
 	@GET
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	public Commune getCommune(@PathParam("code") String code) {
+	public Response getCommune(@PathParam("code") String code) {
 
 		logger.debug("Received GET request for commune " + code);
 
 		Commune commune = new Commune(code);
 		String csvResult = SparqlUtils.executeSparqlQuery(GeoQueries.getCommune(code));
 		CSVUtils.populatePOJO(csvResult, commune);
-		
-		return commune;
+
+		if (commune.getUri() == null) return Response.status(Status.NOT_FOUND).build();
+		return Response.ok(commune).build();
 	}
 
 	@Path("/pays/{code: 99[0-9]{3}}")
 	@GET
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	public Country getCountry(@PathParam("code") String code) {
+	public Response getCountry(@PathParam("code") String code) {
 
 		logger.debug("Received GET request for country " + code);
 
 		Country country = new Country(code);
 		String csvResult = SparqlUtils.executeSparqlQuery(GeoQueries.getCountry(code));
 		CSVUtils.populatePOJO(csvResult, country);
-		
-		return country;
+
+		if (country.getUri() == null) return Response.status(Status.NOT_FOUND).build();
+		return Response.ok(country).build();
 	}
 
 	@Path("/region/{code: [0-9]{2}}")
 	@GET
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	public Region getRegion(@PathParam("code") String code) {
+	public Response getRegion(@PathParam("code") String code) {
 
 		logger.debug("Received GET request for region " + code);
 
 		Region region = new Region(code);
 		String csvResult = SparqlUtils.executeSparqlQuery(GeoQueries.getRegion(code));
 		CSVUtils.populatePOJO(csvResult, region);
-		
-		return region;
+
+		if (region.getUri() == null) return Response.status(Status.NOT_FOUND).build();
+		return Response.ok(region).build();
 	}
 }
