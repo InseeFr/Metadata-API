e2cdb8208198ec2adca248f78b4c1ef9890a3562
Laval Nicolas <nicolas.laval@insee.fr>
2017-09-26 14:19:16 +0200

    Updated CJ and geographic queries

--- src/main/java/fr/insee/rmes/api/codes/cj/CJQueries.java
+++ src/main/java/fr/insee/rmes/api/codes/cj/CJQueries.java
@@ -2,13 +2,21 @@ package fr.insee.rmes.api.codes.cj;
 
 public class CJQueries {
 
-	// TODO Modify query to return only current items
 	public static String getCategorieJuridiqueNiveauIII(String code) {
 		return "SELECT ?uri ?intitule WHERE { \n"
 				+ "?uri skos:notation '" + code + "' . \n"
-				+ "<http://id.insee.fr/codes/cj/n3> skos:member ?uri . \n"
+				+ "?lastCJThirdLevel skos:member ?uri . \n"
 				+ "?uri skos:prefLabel ?intitule  \n"
 				+ "FILTER (lang(?intitule) = 'fr') \n"
+				+ "{ \n"
+				+ "SELECT ?lastCJThirdLevel WHERE { \n"
+				+ "?lastCJThirdLevel xkos:organizedBy <http://id.insee.fr/concepts/cj/cjNiveauIII> . \n"
+				+ "BIND(STRBEFORE(STRAFTER(STR(?lastThirdLevel ),'http://id.insee.fr/codes/cj/cj'), '/niveauIII') AS ?lastCJVersion) \n"
+				+ "BIND(xsd:float(?lastCJVersion) AS ?lastCJVersionFloat)"
+				+ "} \n"
+				+ "ORDER BY DESC (?lastCJVersionFloat) \n"
+				+ "LIMIT 1 \n"
+				+ "} \n"
 				+ "}";
 	}	
 
--- src/main/java/fr/insee/rmes/api/geo/GeoQueries.java
+++ src/main/java/fr/insee/rmes/api/geo/GeoQueries.java
@@ -5,7 +5,7 @@ public class GeoQueries {
 	public static String getCommune(String code) {
 		return "SELECT ?uri ?intitule WHERE { \n"
 				+ "?uri rdf:type igeo:Commune . \n"
-				+ "?uri igeo:codeINSEE '" + code + "' . \n"
+				+ "?uri igeo:codeINSEE '" + code + "'^^xsd:token . \n"
 				+ "?uri igeo:nom ?intitule \n"
 				+ "}";
 	}
@@ -13,7 +13,7 @@ public class GeoQueries {
 	public static String getCountry(String code) {
 		return "SELECT ?uri ?intitule WHERE { \n"
 				+ "?uri rdf:type igeo:Pays . \n"
-				+ "?uri igeo:codeINSEE '" + code + "' . \n"
+				+ "?uri igeo:codeINSEE '" + code + "'^^xsd:token . \n"
 				+ "?uri igeo:nom ?intitule \n"
 				+ "FILTER (lang(?intitule) = 'fr') \n"
 				+ "}";
@@ -22,8 +22,9 @@ public class GeoQueries {
 	public static String getRegion(String code) {
 		return "SELECT ?uri ?intitule WHERE { \n"
 				+ "?uri rdf:type igeo:Region . \n"
-				+ "?uri igeo:codeINSEE '" + code + "' . \n"
+				+ "?uri igeo:codeINSEE '" + code + "'^^xsd:token . \n"
 				+ "?uri igeo:nom ?intitule \n"
+				+ "FILTER (lang(?intitule) = 'fr') \n"
 				+ "}";
 	}
 
