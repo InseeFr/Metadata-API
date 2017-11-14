0f3126211336a43c309dc1237e023eaf50c46bf7
NicoLaval <nicolas.laval@insee.fr>
2017-11-14 11:59:47 +0100

    Corrected geo queries

--- src/main/java/fr/insee/rmes/api/geo/GeoQueries.java
+++ src/main/java/fr/insee/rmes/api/geo/GeoQueries.java
@@ -7,14 +7,20 @@ public class GeoQueries {
 				+ "?uri rdf:type igeo:Commune . \n"
 				+ "?uri igeo:codeINSEE '" + code + "'^^xsd:token . \n"
 				+ "?uri igeo:nom ?intitule \n"
+				// Ensure that is not the IGN URI
+				+ "FILTER (REGEX(STR(?uri), 'id.insee.fr')) \n"
+				+ "FILTER (lang(?intitule) = 'fr') \n"
 				+ "}";
 	}
 	
 	public static String getCountry(String code) {
 		return "SELECT ?uri ?intitule WHERE { \n"
-				+ "?uri rdf:type igeo:Etat . \n"
+				+ "VALUES ?country { igeo:TerritoireAutonomeOuASouveraineteSpeciale igeo:Etat igeo:Pays } \n"
+				+ "?uri rdf:type ?country . \n"
 				+ "?uri igeo:codeINSEE '" + code + "'^^xsd:token . \n"
 				+ "?uri igeo:nom ?intitule \n"
+				// Ensure that is not the dbpedia URI
+				+ "FILTER (REGEX(STR(?uri), 'id.insee.fr')) \n"
 				+ "FILTER (lang(?intitule) = 'fr') \n"
 				+ "}";
 	}
@@ -24,6 +30,8 @@ public class GeoQueries {
 				+ "?uri rdf:type igeo:Region . \n"
 				+ "?uri igeo:codeINSEE '" + code + "'^^xsd:token . \n"
 				+ "?uri igeo:nom ?intitule \n"
+				// Ensure that is not the IGN URI
+				+ "FILTER (REGEX(STR(?uri), 'id.insee.fr')) \n"
 				+ "FILTER (lang(?intitule) = 'fr') \n"
 				+ "}";
 	}
