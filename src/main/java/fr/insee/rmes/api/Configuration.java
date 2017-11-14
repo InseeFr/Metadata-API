c7d3c10966475495fd8ba51f12e181c136e78c7e
NicoLaval <nicolas.laval@insee.fr>
2017-11-14 12:19:30 +0100

    Fix minor issue with endpoint configuration

--- src/main/java/fr/insee/rmes/api/Configuration.java
+++ src/main/java/fr/insee/rmes/api/Configuration.java
@@ -3,6 +3,6 @@ package fr.insee.rmes.api;
 public class Configuration {
 
 	// TODO Externalize props
-	public final static String SPARQL_END_POINT = "http://id.insee.fr/sparql";
-	
+	public final static String SPARQL_END_POINT = "http://qfrmessnczlht01.ad.insee.intra/sparql";
+	//public final static String SPARQL_END_POINT = "http://id.insee.fr/sparql";
 }
