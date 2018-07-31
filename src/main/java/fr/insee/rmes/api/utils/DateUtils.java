f131477ec145473ea8660e5f06a036c797da4358
Nicolas Laval <nicolas.laval@insee.fr>
2018-07-31 11:27:12 +0200

    Update date format

--- src/main/java/fr/insee/rmes/api/utils/DateUtils.java
+++ src/main/java/fr/insee/rmes/api/utils/DateUtils.java
@@ -10,15 +10,15 @@ import org.joda.time.format.DateTimeFormatter;
 public class DateUtils {
 	
 	public static DateTime getDateTimeFromString(String date) {
-		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy/MM/dd");
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
 		DateTime dt = formatter.parseDateTime(date);
 		return dt;
 	}
 
 	public static boolean isValidDate(String date) {
-		if (date == null || !date.matches("\\d{4}/[01]\\d/[0-3]\\d"))
+		if (date == null || !date.matches("\\d{4}-[01]\\d-[0-3]\\d"))
 	        return false;
-		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 	     try {
 	          format.parse(date);
 	          return true;
