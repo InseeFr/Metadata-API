c46de1c05502a83be3afac7f7a4ba88a334a375e
Nicolas Laval <nicolas.laval@insee.fr>
2018-07-31 12:01:30 +0200

    Fix NA1973 query

--- src/main/java/fr/insee/rmes/api/codes/na1973/Na1973Queries.java
+++ src/main/java/fr/insee/rmes/api/codes/na1973/Na1973Queries.java
@@ -7,7 +7,7 @@ public class Na1973Queries {
 	public static String getGroupeNA1973(String code) {
 		return "SELECT ?uri ?intitule WHERE { \n"
 				+ "?uri skos:notation '" + code + "' . \n"
-				+ "<" + Configuration.BASE_HOST + "/codes/na1973/groupes> skos:member ?uri . \n"
+				+ "<" + Configuration.BASE_HOST + "/codes/na73/groupes> skos:member ?uri . \n"
 				+ "?uri skos:prefLabel ?intitule  \n"
 				+ "FILTER (lang(?intitule) = 'fr') \n"
 				+ "}";
