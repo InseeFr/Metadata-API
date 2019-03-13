8d86849cdcc116ec42538375be63dfd42577289a
Rémy Capot <remy.capot@insee.fr>
2019-03-13 15:45:32 +0100

    Update Configuration.java

--- src/main/java/fr/insee/rmes/config/Configuration.java
+++ src/main/java/fr/insee/rmes/config/Configuration.java
@@ -5,7 +5,7 @@ public class Configuration {
 	// TODO Externalize props
 	//public final static String SPARQL_END_POINT = "http://qfrmessnczlht01.ad.insee.intra/sparql";
 	//public final static String BASE_HOST = "http://qfrmessnczlht01";
-	//public final static String SPARQL_END_POINT = "http://rdf.insee.fr/sparql";
+	//public final static String SPARQL_END_POINT = "http://id.insee.fr/sparql";
 	//public final static String BASE_HOST = "http://id.insee.fr";
 	public final static String SPARQL_END_POINT = "http://dvrmessnczlht01.ad.insee.intra/sparql";
 	public final static String BASE_HOST = "http://dvrmessnczlht01/datalift";
