76f5caff1987676e1dd2f0e4eb5253ae3a2160d6
Alice Lambois <alice.lambois@insee.fr>
2019-04-12 13:47:10 +0200

    Init documentation service

--- src/main/java/fr/insee/rmes/api/operations/OperationsAPI.java
+++ src/main/java/fr/insee/rmes/api/operations/OperationsAPI.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -17,6 +18,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import fr.insee.rmes.api.operations.documentations.CsvRubrique;
+import fr.insee.rmes.api.operations.documentations.Document;
+import fr.insee.rmes.api.operations.documentations.DocumentationSims;
+import fr.insee.rmes.api.operations.documentations.Rubrique;
 import fr.insee.rmes.api.utils.CSVUtils;
 import fr.insee.rmes.api.utils.ResponseUtils;
 import fr.insee.rmes.api.utils.SparqlUtils;
@@ -74,7 +79,57 @@ public class OperationsAPI {
 		
 		
 	}
+	
+	@SuppressWarnings("unchecked")
+	@Path("/documentation/{id: [0-9]{4}}")
+	@GET
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Response getDocumentation(@HeaderParam("Accept") String header, @PathParam("id") String id) {
+		logger.debug("Received GET request documentation");
+		
+		String csvResult = SparqlUtils.executeSparqlQuery(OperationsQueries.getDocumentationTitle(id));
+		DocumentationSims sims =  new DocumentationSims();
+		CSVUtils.populatePOJO(csvResult, sims);
+				
+		if (sims.getUri() == null) return Response.status(Status.NOT_FOUND).entity("").build();
+		
+		csvResult = SparqlUtils.executeSparqlQuery(OperationsQueries.getDocumentationRubrics(id));
+		List<CsvRubrique> csvRubriques = (List<CsvRubrique>) CSVUtils.populateMultiPOJO(csvResult, CsvRubrique.class);
+		List<Rubrique> rubriques = new ArrayList<>();
+		for (CsvRubrique cr : csvRubriques) {
+			Rubrique r = new Rubrique(cr.getId(),cr.getUri(),cr.getType());
+			r.setTitre(cr.getTitreLg1(),cr.getTitreLg2());
+			r.setIdParent(cr.getIdParent());
+			switch (cr.getType()) {
+			case "DATE":
+				r.setValeurSimple(cr.getValeurSimple());
+				break;
+			case "CODE_LIST":
+				SimpleObject valeurCode = new SimpleObject(cr.getValeurSimple(), cr.getCodeUri(), cr.getLabelObjLg1(), cr.getLabelObjLg2());
+				r.setValeurCode(valeurCode);
+				break;	
+			case "ORGANISATION":
+				SimpleObject valeurOrg = new SimpleObject(cr.getValeurSimple(), cr.getOrganisationUri(), cr.getLabelObjLg1(), cr.getLabelObjLg2());
+				r.setValeurOrganisation(valeurOrg);
+				break;	
+			case "RICH_TEXT":
+				String csvDocs = SparqlUtils.executeSparqlQuery(OperationsQueries.getDocuments(id,r.getId()));
+				List<Document> docs = (List<Document>) CSVUtils.populateMultiPOJO(csvDocs, Document.class);
+				r.setDocuments(docs);
+			case "TEXT":
+				r.setLabelLg1(cr.getLabelLg1());
+				r.setLabelLg2(cr.getLabelLg2());
+				break;	
 
+			default:
+				break;
+			}
+			rubriques.add(r);
+		}
+		sims.setRubriques(rubriques);
+		
+		return Response.ok(ResponseUtils.produceResponse(sims, header)).build();
+	}
 
 }
 
--- src/main/java/fr/insee/rmes/api/operations/OperationsQueries.java
+++ src/main/java/fr/insee/rmes/api/operations/OperationsQueries.java
@@ -30,7 +30,7 @@ public class OperationsQueries {
 				"		FILTER (lang(?opLabelLg2) = 'en') \r\n" + 
 				"		BIND(STRAFTER(STR(?operation),'/operations/operation/') AS ?operationId) . \r\n" + 
 				"		OPTIONAL { ?sims sdmx-mm:target ?operation . \r\n" + 
-				"			   ?sims a sdmx-mm:MetadataReport . \r\n" + 
+				"			 ?sims a sdmx-mm:MetadataReport . \r\n" + 
 				"				BIND(STRAFTER(STR(?sims),'/qualite/rapport/') AS ?simsId) . \r\n" + 
 				"		}\r\n" + 
 				"	}}\r\n" + 
@@ -43,7 +43,7 @@ public class OperationsQueries {
 				"		FILTER (lang(?indicLabelLg2) = 'en') \r\n" + 
 				"		BIND(STRAFTER(STR(?indic),'/produits/indicateur/') AS ?indicId) . \r\n" + 
 				"		OPTIONAL { ?sims sdmx-mm:target ?indic . \r\n" + 
-				"			   ?sims a sdmx-mm:MetadataReport . \r\n" + 
+				"			 ?sims a sdmx-mm:MetadataReport . \r\n" + 
 				"				BIND(STRAFTER(STR(?sims),'/qualite/rapport/') AS ?simsId) . \r\n" + 
 				"		}\r\n" + 
 				"	}}\r\n" + 
@@ -55,6 +55,165 @@ public class OperationsQueries {
 				" order by ?familyId ?seriesId ";
 			
 	}
+	
+	public static String getDocumentationTitle(String idSims) {
+		return " SELECT ?id ?uri ?labelLg1 ?labelLg2 ?idCible ?cible ?labelCibleLg1 ?labelCibleLg2 "
+				+" FROM <http://rdf.insee.fr/graphes/qualite/rapport/"+idSims+"> "
+				+" WHERE { "
+				+" 	?uri rdf:type sdmx-mm:MetadataReport .   "
+				+" 		 "
+				+" 	 OPTIONAL{ ?uri sdmx-mm:target ?cible .  "
+				+ " BIND(STRAFTER(STR(?cible),'/') AS ?idCible) . "
+				+" 			  ?cible rdfs:label ?labelCibleLg1 . "
+				+" 			  FILTER(lang(?labelCibleLg1) = 'fr') "
+				+" 			  OPTIONAL{?cible rdfs:label ?labelCibleLg2 . "
+				+" 			  FILTER(lang(?labelCibleLg2) = 'en') }  "
+				+" } "
+				+" 				 "
+				+" 	 OPTIONAL{ ?uri rdfs:label ?labelLg1 .  "
+				+" 		FILTER(lang(?labelLg1) = 'fr') "
+				+" 	 } "
+				+" 	 OPTIONAL{ ?uri rdfs:label ?labelLg2 .  "
+				+" 		FILTER(lang(?labelLg2) = 'en') "
+				+" 	 } "
+				+" 				 "
+				+" 	FILTER(STRENDS(STR(?uri), '"+idSims+"')) "
+				+ " BIND ("+idSims+" AS ?id )  "
+				+" } ";
+	}
 
-
+	public static String getDocumentationRubrics(String idSims) {
+		return "SELECT ?uri ?id ?idParent ?titreLg1 ?titreLg2 ?type ?valeurSimple ?labelLg1 ?labelLg2 ?codeUri ?organisationUri ?hasDoc ?labelObjLg1 ?labelObjLg2      "
+				+"	FROM <http://rdf.insee.fr/graphes/qualite/rapport/"+idSims+"> "
+				+"	FROM <http://rdf.insee.fr/graphes/codes> "
+				+"	FROM <http://rdf.insee.fr/graphes/organisations> "
+				+"	FROM <http://rdf.insee.fr/graphes/def/simsv2fr> "
+				+"	FROM <http://rdf.insee.fr/graphes/concepts/qualite> "
+				+"WHERE { "
+				+"	{ "
+				+"		?report rdf:type sdmx-mm:MetadataReport . "
+				+"		?reporturi sdmx-mm:metadataReport ?report . "
+				+"		OPTIONAL {?mas sdmx-mm:metadataAttributeProperty ?uri . "
+				+"			?mas sdmx-mm:parent ?uriParent . } "
+				+"		BIND(REPLACE( STR(?uriParent) , '(.*/)(\\\\w.+$)', '$2' ) AS ?idParent) . "
+				+"		?uri sdmx-mm:concept ?concept . "
+				+"		?concept skos:prefLabel ?titreLg1 ; skos:prefLabel ?titreLg2 ; "
+				+"		FILTER(lang(?titreLg1) = 'fr') "
+				+"		FILTER(lang(?titreLg2) = 'en') "
+				+"		BIND(REPLACE( STR(?uri) , '(.*/)(\\\\w.+$)', '$2' ) AS ?id) . "
+				+"		?reporturi ?uri ?valeurSimple . "
+				+"		FILTER ( datatype(?valeurSimple) = xsd:date ) "
+				+"		BIND('DATE' AS ?type) . "
+				+"	} "
+				+"				 "
+				+"	UNION { "
+				+"		?report rdf:type sdmx-mm:MetadataReport . "
+				+"		?reporturi sdmx-mm:metadataReport ?report . "
+				+"		OPTIONAL {			?mas sdmx-mm:metadataAttributeProperty ?uri . "
+				+"			?mas sdmx-mm:parent ?uriParent . } "
+				+"		BIND(REPLACE( STR(?uriParent) , '(.*/)(\\\\w.+$)', '$2' ) AS ?idParent) . "
+				+"		?uri sdmx-mm:concept ?concept . "
+				+"		?concept skos:prefLabel ?titreLg1 ; "
+				+" 		 skos:prefLabel ?titreLg2 ; "
+				+"		FILTER(lang(?titreLg1) = 'fr') "
+				+"		FILTER(lang(?titreLg2) = 'en') "
+				+"		?reporturi ?uri ?text . "
+				+"		?text rdf:type <http://purl.org/dc/dcmitype/Text> "
+				+"		BIND(REPLACE( STR(?uri) , '(.*/)(\\\\w.+$)', '$2' ) AS ?id) . "
+				+"		OPTIONAL{ ?text rdf:value ?labelLg1 . "
+				+"			FILTER(lang(?labelLg1) = 'fr') "
+				+"		} "
+				+"		OPTIONAL{?text rdf:value ?labelLg2 . "
+				+"			FILTER(lang(?labelLg2) = 'en') "
+				+"		} "
+				+"		BIND(EXISTS{?text insee:additionalMaterial ?doc} AS ?hasDoc) "
+				+"		BIND('RICH_TEXT' AS ?type) . "
+				+"	} "
+				+"			 "
+				+"	UNION { "
+				+"		?report rdf:type sdmx-mm:MetadataReport . "
+				+"		?reporturi sdmx-mm:metadataReport ?report . "
+				+"		OPTIONAL { ?mas sdmx-mm:metadataAttributeProperty ?uri . "
+				+"			?mas sdmx-mm:parent ?uriParent . } "
+				+"		BIND(REPLACE( STR(?uriParent) , '(.*/)(\\\\w.+$)', '$2' ) AS ?idParent) . "
+				+"		?uri sdmx-mm:concept ?concept . "
+				+"		?concept skos:prefLabel ?titreLg1 ; skos:prefLabel ?titreLg2 ; "
+				+"		FILTER(lang(?titreLg1) = 'fr') "
+				+"		FILTER(lang(?titreLg2) = 'en') "
+				+"		BIND(REPLACE( STR(?uri) , '(.*/)(\\\\w.+$)', '$2' ) AS ?id) . "
+				+"		?reporturi ?uri ?labelLg1 . "
+				+"		FILTER(lang(?labelLg1) = 'fr') "
+				+"		OPTIONAL{?reporturi ?uri ?labelLg2 . "
+				+"			FILTER(lang(?labelLg2) = 'en') } "
+				+"		BIND('TEXT' AS ?type) . "
+				+"	} 		 "
+				+"	UNION { "
+				+"		?report rdf:type sdmx-mm:MetadataReport . "
+				+"		?reporturi sdmx-mm:metadataReport ?report . "
+				+"		OPTIONAL {?mas sdmx-mm:metadataAttributeProperty ?uri . "
+				+"			?mas sdmx-mm:parent ?uriParent . } "
+				+"		BIND(REPLACE( STR(?uriParent) , '(.*/)(\\\\w.+$)', '$2' ) AS ?idParent) . "
+				+"		?uri sdmx-mm:concept ?concept . "
+				+"		?concept skos:prefLabel ?titreLg1 ; skos:prefLabel ?titreLg2 ; "
+				+"		FILTER(lang(?titreLg1) = 'fr') "
+				+"		FILTER(lang(?titreLg2) = 'en') "
+				+"		BIND(REPLACE( STR(?uri) , '(.*/)(\\\\w.+$)', '$2' ) AS ?id) . "
+				+"		?reporturi ?uri ?codeUri . "
+				+"		?codeUri skos:notation ?valeurSimple . "
+				+"		?codeUri skos:inScheme ?listUri . "
+				+"		?listUri skos:notation ?codeList . "
+				+"		?codeUri skos:prefLabel ?labelObjLg1 . "
+				+"		FILTER (lang(?labelObjLg1) = 'fr') . "
+				+"		?codeUri skos:prefLabel ?labelObjLg2 . "
+				+"		FILTER (lang(?labelObjLg2) = 'en') . "
+				+"		BIND('CODE_LIST' AS ?type) . "
+				+"	} "
+				+"	UNION { "
+				+"		?report rdf:type sdmx-mm:MetadataReport . "
+				+"		?reporturi sdmx-mm:metadataReport ?report . "
+				+"		OPTIONAL { ?mas sdmx-mm:metadataAttributeProperty ?uri . "
+				+"			?mas sdmx-mm:parent ?uriParent . } "
+				+"		BIND(REPLACE( STR(?uriParent) , '(.*/)(\\\\w.+$)', '$2' ) AS ?idParent) . "
+				+"		?uri sdmx-mm:concept ?concept . "
+				+"		?concept skos:prefLabel ?titreLg1 ; skos:prefLabel ?titreLg2 ; "
+				+"		FILTER(lang(?titreLg1) = 'fr') "
+				+"		FILTER(lang(?titreLg2) = 'en') "
+				+"		?reporturi ?uri ?organisationUri . "
+				+"		BIND(REPLACE( STR(?uri) , '(.*/)(\\\\w.+$)', '$2' ) AS ?id) . "
+				+"		?organisationUri dcterms:identifier ?valeurSimple . "
+				+"		OPTIONAL { ?organisationUri skos:prefLabel ?labelObjLg1 . "
+				+"			FILTER (lang(?labelObjLg1) = 'fr')} "
+				+"		OPTIONAL {?organisationUri skos:prefLabel ?labelObjLg2 . "
+				+"			FILTER (lang(?labelObjLg2) = 'en') } "
+				+"		BIND('ORGANISATION' AS ?type) . "
+				+"	} "
+				+"} ";
+	}
+	
+	public static String getDocuments(String idSims, String idRubric) {
+		return "SELECT ?url ?labelLg1 ?labelLg2 ?dateMiseAJour ?langue "
+				+"FROM <http://rdf.insee.fr/graphes/qualite/rapport/"+idSims+"> "
+				+"WHERE { "
+				+"	 ?text rdf:type dcmitype:Text . "
+				+"	 ?text insee:additionalMaterial ?document . "
+				+"	 ?document rdf:type foaf:Document . "
+				+"	 "
+				+"	 ?document <http://schema.org/url> ?url "
+				+" "
+				+" 	 OPTIONAL{ ?document rdfs:label ?labelLg1 . "
+				+"		FILTER(lang(?labelLg1) = 'fr') "
+				+"	 } "
+				+"	 OPTIONAL{ ?document rdfs:label ?labelLg2 . "
+				+"		FILTER(lang(?labelLg2) = 'en') "
+				+"	 } "
+				+"	 "
+				+"	 OPTIONAL{ ?document pav:lastRefreshedOn ?dateMiseAJour . } "
+				+"	 OPTIONAL{ ?document dc:language ?langue . } "
+				+"	 "
+				+"	FILTER(REGEX(STR(?text), '"+idRubric+"')) "
+				+" "
+				+" } ";
+	}
+		
+	
 }
