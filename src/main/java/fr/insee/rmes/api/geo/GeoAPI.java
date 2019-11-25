8519eb6d7503b0131fadeeb4c79f97140b061fbc
Sophie Marziali <sophie.marziali@insee.fr>
2019-11-25 15:53:56 +0100

    refactor GeoApi + unit tests

--- src/main/java/fr/insee/rmes/api/geo/GeoAPI.java
+++ src/main/java/fr/insee/rmes/api/geo/GeoAPI.java
@@ -5,6 +5,7 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -17,9 +18,15 @@ import fr.insee.rmes.modeles.geo.Commune;
 import fr.insee.rmes.modeles.geo.Country;
 import fr.insee.rmes.modeles.geo.Region;
 import fr.insee.rmes.queries.geo.GeoQueries;
-import fr.insee.rmes.utils.ResponseUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Path("/geo")
+@Tag(name = "géographie", description = "Geographie API")
 public class GeoAPI extends MetadataApi {
 
     private static Logger logger = LogManager.getLogger(GeoAPI.class);
@@ -29,16 +36,32 @@ public class GeoAPI extends MetadataApi {
     @Produces({
         MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML
     })
-    public Response getCommune(@PathParam("code") String code, @HeaderParam("Accept") String header) {
+    @Operation(
+        operationId = "getcogcom",
+        summary = "Informations sur une commune française identifiée par son code (cinq caractères)",
+        description = "Cette requête renvoie également les communes des collectivités d'Outre-Mer",
+        responses = {
+            @ApiResponse(content = @Content(schema = @Schema(implementation = Commune.class)))
+        })
+    public Response getCommune(
+        @Parameter(
+            description = "Code de la commune (cinq caractères)",
+            required = true,
+            schema = @Schema(pattern = "[0-9][0-9AB][0-9]{3}", type = "string")) @PathParam("code") String code,
+        @Parameter(hidden = true) @HeaderParam(HttpHeaders.ACCEPT) String header) {
 
         logger.debug("Received GET request for commune " + code);
 
         Commune commune = new Commune(code);
         String csvResult = sparqlUtils.executeSparqlQuery(GeoQueries.getCommune(code));
-        csvUtils.populatePOJO(csvResult, commune);
+        commune = (Commune) csvUtils.populatePOJO(csvResult, commune);
 
-        if (commune.getUri() == null) return Response.status(Status.NOT_FOUND).entity("").build();
-        return Response.ok(responseUtils.produceResponse(commune, header)).build();
+        if (commune.getUri() == null) {
+            return Response.status(Status.NOT_FOUND).entity("").build();
+        }
+        else {
+            return Response.ok(responseUtils.produceResponse(commune, header)).build();
+        }
     }
 
     @Path("/pays/{code: 99[0-9]{3}}")
@@ -46,16 +69,32 @@ public class GeoAPI extends MetadataApi {
     @Produces({
         MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML
     })
-    public Response getCountry(@PathParam("code") String code, @HeaderParam("Accept") String header) {
+    @Operation(
+        operationId = "getcogpay",
+        summary = "Informations sur un pays identifié par son code (cinq chiffres)",
+        responses = {
+            @ApiResponse(content = @Content(schema = @Schema(implementation = Country.class)))
+        })
+    public Response getCountry(
+        @Parameter(
+            description = "Code du pays (cinq chiffres, débutant par 99)",
+            required = true,
+            schema = @Schema(pattern = "99[0-9]{3}", type = "string")) @PathParam("code") String code,
+        @Parameter(hidden = true) @HeaderParam(HttpHeaders.ACCEPT) String header) {
 
         logger.debug("Received GET request for country " + code);
 
         Country country = new Country(code);
         String csvResult = sparqlUtils.executeSparqlQuery(GeoQueries.getCountry(code));
-        csvUtils.populatePOJO(csvResult, country);
+        country = (Country) csvUtils.populatePOJO(csvResult, country);
 
-        if (country.getUri() == null) return Response.status(Status.NOT_FOUND).entity("").build();
-        return Response.ok(responseUtils.produceResponse(country, header)).build();
+        if (country.getUri() == null) {
+            return Response.status(Status.NOT_FOUND).entity("").build();
+        }
+        else {
+            return Response.ok(responseUtils.produceResponse(country, header)).build();
+        }
+
     }
 
     @Path("/region/{code: [0-9]{2}}")
@@ -63,15 +102,30 @@ public class GeoAPI extends MetadataApi {
     @Produces({
         MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML
     })
-    public Response getRegion(@PathParam("code") String code, @HeaderParam("Accept") String header) {
+    @Operation(
+        operationId = "getcogreg",
+        summary = "Informations sur une région française identifiée par son code (deux chiffres)",
+        responses = {
+            @ApiResponse(content = @Content(schema = @Schema(implementation = Region.class)))
+        })
+    public Response getRegion(
+        @Parameter(
+            description = "Code de la région (deux chiffres)",
+            required = true,
+            schema = @Schema(pattern = "[0-9]{2}", type = "string")) @PathParam("code") String code,
+        @Parameter(hidden = true) @HeaderParam(HttpHeaders.ACCEPT) String header) {
 
         logger.debug("Received GET request for region " + code);
 
         Region region = new Region(code);
         String csvResult = sparqlUtils.executeSparqlQuery(GeoQueries.getRegion(code));
-        csvUtils.populatePOJO(csvResult, region);
+        region = (Region) csvUtils.populatePOJO(csvResult, region);
 
-        if (region.getUri() == null) return Response.status(Status.NOT_FOUND).entity("").build();
-        return Response.ok(responseUtils.produceResponse(region, header)).build();
+        if (region.getUri() == null) {
+            return Response.status(Status.NOT_FOUND).entity("").build();
+        }
+        else {
+            return Response.ok(responseUtils.produceResponse(region, header)).build();
+        }
     }
 }
---
+++ src/test/java/fr/insee/rmes/api/geo/GeoAPITest.java
@@ -0,0 +1,192 @@
+package fr.insee.rmes.api.geo;
+
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import fr.insee.rmes.modeles.geo.Commune;
+import fr.insee.rmes.modeles.geo.Country;
+import fr.insee.rmes.modeles.geo.Region;
+import fr.insee.rmes.utils.CSVUtils;
+import fr.insee.rmes.utils.ResponseUtils;
+import fr.insee.rmes.utils.SparqlUtils;
+
+@ExtendWith(MockitoExtension.class)
+public class GeoAPITest {
+
+    @InjectMocks
+    private GeoAPI geoApi;
+
+    @Mock
+    private SparqlUtils mockSparqlUtils;
+
+    @Mock
+    private CSVUtils mockCSVUtils;
+
+    @Mock
+    private ResponseUtils mockResponseUtils;
+
+    private Commune commune = new Commune();
+    private Country country = new Country();
+    private Region region = new Region();
+
+    @Before
+    public void init() {
+
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void givenGetCommune_whenCorrectRequest_andHeaderContentIsJson_thenResponseIsOk() {
+
+        // Mock methods
+        commune.setUri("quelquechose");
+        when(mockSparqlUtils.executeSparqlQuery(Mockito.any())).thenReturn("");
+        when(mockCSVUtils.populatePOJO(Mockito.anyString(), Mockito.any())).thenReturn(commune);
+        when(mockResponseUtils.produceResponse(Mockito.any(), Mockito.any())).thenReturn(null);
+
+        // Call method
+        geoApi.getCommune("", MediaType.APPLICATION_JSON);
+        verify(mockResponseUtils, times(1)).produceResponse(Mockito.any(), Mockito.any());
+    }
+
+    @Test
+    public void givenGetCommune_whenCorrectRequest_andHeaderContentIsXml_thenResponseIsOk() {
+
+        // Mock methods
+        commune.setUri("quelquechose");
+        when(mockSparqlUtils.executeSparqlQuery(Mockito.any())).thenReturn("");
+        when(mockCSVUtils.populatePOJO(Mockito.anyString(), Mockito.any())).thenReturn(commune);
+        when(mockResponseUtils.produceResponse(Mockito.any(), Mockito.any())).thenReturn(null);
+
+        // Call method
+        geoApi.getCommune("", MediaType.APPLICATION_XML);
+        verify(mockResponseUtils, times(1)).produceResponse(Mockito.any(), Mockito.any());
+    }
+
+    @Test
+    public void givenGetCountry_whenCorrectRequest_andHeaderContentIsJson_thenResponseIsOk() {
+
+        // Call method
+        country.setUri("quelquechose");
+        when(mockSparqlUtils.executeSparqlQuery(Mockito.any())).thenReturn("");
+        when(mockCSVUtils.populatePOJO(Mockito.anyString(), Mockito.any())).thenReturn(country);
+        when(mockResponseUtils.produceResponse(Mockito.any(), Mockito.any())).thenReturn(null);
+
+        // Call method
+        geoApi.getCountry("", MediaType.APPLICATION_JSON);
+        verify(mockResponseUtils, times(1)).produceResponse(Mockito.any(), Mockito.any());
+    }
+
+    @Test
+    public void givenGetCountry_whenCorrectRequest_andHeaderContentIsXml_thenResponseIsOk() {
+
+        // Call method
+        country.setUri("quelquechose");
+        when(mockSparqlUtils.executeSparqlQuery(Mockito.any())).thenReturn("");
+        when(mockCSVUtils.populatePOJO(Mockito.anyString(), Mockito.any())).thenReturn(country);
+        when(mockResponseUtils.produceResponse(Mockito.any(), Mockito.any())).thenReturn(null);
+
+        // Mock methods
+        geoApi.getCountry("", MediaType.APPLICATION_XML);
+        verify(mockResponseUtils, times(1)).produceResponse(Mockito.any(), Mockito.any());
+    }
+
+    @Test
+    public void givenGetRegion_whenCorrectRequestt_andHeaderContentIsJson_thenResponseIsOk() {
+
+        // Mock methods
+        region.setUri("quelquechose");
+        when(mockSparqlUtils.executeSparqlQuery(Mockito.any())).thenReturn("");
+        when(mockCSVUtils.populatePOJO(Mockito.anyString(), Mockito.any())).thenReturn(region);
+        when(mockResponseUtils.produceResponse(Mockito.any(), Mockito.any())).thenReturn(null);
+
+        // Call method
+        geoApi.getRegion("", MediaType.APPLICATION_JSON);
+        verify(mockResponseUtils, times(1)).produceResponse(Mockito.any(), Mockito.any());
+    }
+
+    @Test
+    public void givenGetRegion_whenCorrectRequestt_andHeaderContentIsXml_thenResponseIsOk() {
+
+        // Mock methods
+        region.setUri("quelquechose");
+        when(mockSparqlUtils.executeSparqlQuery(Mockito.any())).thenReturn("");
+        when(mockCSVUtils.populatePOJO(Mockito.anyString(), Mockito.any())).thenReturn(region);
+        when(mockResponseUtils.produceResponse(Mockito.any(), Mockito.any())).thenReturn(null);
+
+        // Call method
+        geoApi.getRegion("", MediaType.APPLICATION_XML);
+        verify(mockResponseUtils, times(1)).produceResponse(Mockito.any(), Mockito.any());
+    }
+
+    @Test
+    public void givenGetCommune_WhenCorrectRequest_thenResponseIsNotFound() {
+
+        // Mock methods
+        when(mockSparqlUtils.executeSparqlQuery(Mockito.any())).thenReturn("");
+        when(mockCSVUtils.populatePOJO(Mockito.anyString(), Mockito.any())).thenReturn(new Commune());
+
+        // Call method header content = xml
+        Response response = geoApi.getCommune("", MediaType.APPLICATION_XML);
+        verify(mockResponseUtils, never()).produceResponse(Mockito.any(), Mockito.any());
+        Assertions.assertEquals(Status.NOT_FOUND.getStatusCode(), response.getStatus());
+
+        // Call method header content = json
+        response = geoApi.getCommune("", MediaType.APPLICATION_JSON);
+        verify(mockResponseUtils, never()).produceResponse(Mockito.any(), Mockito.any());
+        Assertions.assertEquals(Status.NOT_FOUND.getStatusCode(), response.getStatus());
+    }
+
+    @Test
+    public void givenGetCountry_WhenCorrectRequest_thenResponseIsNotFound() {
+
+        // Mock methods
+        when(mockSparqlUtils.executeSparqlQuery(Mockito.any())).thenReturn("");
+        when(mockCSVUtils.populatePOJO(Mockito.anyString(), Mockito.any())).thenReturn(new Country());
+
+        // Call method header content = xml
+        Response response = geoApi.getCountry("", MediaType.APPLICATION_XML);
+        verify(mockResponseUtils, never()).produceResponse(Mockito.any(), Mockito.any());
+        Assertions.assertEquals(Status.NOT_FOUND.getStatusCode(), response.getStatus());
+
+        // Call method header content = json
+        response = geoApi.getCountry("", MediaType.APPLICATION_JSON);
+        verify(mockResponseUtils, never()).produceResponse(Mockito.any(), Mockito.any());
+        Assertions.assertEquals(Status.NOT_FOUND.getStatusCode(), response.getStatus());
+    }
+
+    @Test
+    public void givenGetRegion_WhenCorrectRequest_thenResponseIsNotFound() {
+
+        // Mock methods
+        when(mockSparqlUtils.executeSparqlQuery(Mockito.any())).thenReturn("");
+        when(mockCSVUtils.populatePOJO(Mockito.anyString(), Mockito.any())).thenReturn(new Region());
+
+        // Call method header content = xml
+        Response response = geoApi.getRegion("", MediaType.APPLICATION_XML);
+        verify(mockResponseUtils, never()).produceResponse(Mockito.any(), Mockito.any());
+        Assertions.assertEquals(Status.NOT_FOUND.getStatusCode(), response.getStatus());
+
+        // Call method header content = json
+        response = geoApi.getRegion("", MediaType.APPLICATION_JSON);
+        verify(mockResponseUtils, never()).produceResponse(Mockito.any(), Mockito.any());
+        Assertions.assertEquals(Status.NOT_FOUND.getStatusCode(), response.getStatus());
+    }
+
+}
