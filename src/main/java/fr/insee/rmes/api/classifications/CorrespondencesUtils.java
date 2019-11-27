75e4305c0846e9eb0f673ceaa6ea3bdc25bfc9be
Alice Lambois <alice.lambois@insee.fr>
2019-11-27 15:25:40 +0100

    Init refactor

--- src/main/java/fr/insee/rmes/api/classifications/CorrespondencesUtils.java
+++ src/main/java/fr/insee/rmes/api/classifications/CorrespondencesUtils.java
@@ -46,53 +46,18 @@ public class CorrespondencesUtils {
      */
     public static Map<Poste, List<Poste>> getTreeMapTargetItemsBySourceByCorrespondenceId(
         List<RawCorrespondence> rawItemsList) {
-
         /* TreeMap for ordering map keys */
         Map<Poste, List<Poste>> groupedListItems = new TreeMap<Poste, List<Poste>>();
         for (RawCorrespondence curRawCorrespondence : rawItemsList) {
-
-            Poste posteSource =
-                new Poste(
-                    curRawCorrespondence.getCodePoste1(),
-                    curRawCorrespondence.getUriPoste1(),
-                    curRawCorrespondence.getIntituleFrPoste1(),
-                    curRawCorrespondence.getIntituleEnPoste1());
-
-            if ( ! groupedListItems.containsKey(posteSource)) { // add source and target items in map if new item source
-
-                // code source item is the map of the key whitch representing a 1 to many correspondences from a target
-                // code
+            Poste posteSource = newPoste1(curRawCorrespondence);
+            if ( ! groupedListItems.containsKey(posteSource)) { // initialize map if new item source
                 groupedListItems.put(posteSource, new ArrayList<Poste>());
-
-                // add targetItem
-                Poste targetPoste =
-                    new Poste(
-                        curRawCorrespondence.getCodePoste2(),
-                        curRawCorrespondence.getUriPoste2(),
-                        curRawCorrespondence.getIntituleFrPoste2(),
-                        curRawCorrespondence.getIntituleEnPoste2());
-
-                groupedListItems.get(posteSource).add(targetPoste);
-
             }
-
-            else {
-
-                // add targetItem only in map
-                Poste targetPoste =
-                    new Poste(
-                        curRawCorrespondence.getCodePoste2(),
-                        curRawCorrespondence.getUriPoste2(),
-                        curRawCorrespondence.getIntituleFrPoste2(),
-                        curRawCorrespondence.getIntituleEnPoste2());
-
-                groupedListItems.get(posteSource).add(targetPoste);
-
-            }
+            // add targetItem
+            Poste targetPoste = newPoste2(curRawCorrespondence);
+            groupedListItems.get(posteSource).add(targetPoste);
         }
-
         return groupedListItems;
-
     }
 
     /* when id1 + id2 classifications */
@@ -126,33 +91,17 @@ public class CorrespondencesUtils {
          */
         /* if false => first fields of csv result are for source classification item */
         /* if true => second part of fields */
-        boolean mustSwapCorrespondences = shouldSwapRawCorespondences(codeClassificationSource, rawItemsList);
+        boolean poste1IsSource = isPoste1Source(codeClassificationSource, rawItemsList);
 
         for (RawCorrespondence curRawCorrespondence : rawItemsList) {
-
-            Poste posteSource = mapRawObjectToItemCorrespondence(curRawCorrespondence, mustSwapCorrespondences);
-
-            if ( ! groupedListItems.containsKey(posteSource)) {
-
-                // code source item is the map of the key whitch representing a 1 to many correspondences from a target
-                // code
+            Poste posteSource = mapRawObjectToItemCorrespondence(curRawCorrespondence, poste1IsSource);
+            if ( ! groupedListItems.containsKey(posteSource)) {// initialize map
                 groupedListItems.put(posteSource, new ArrayList<Poste>());
-
-                // add targetItem
-                Poste targetPoste = mapRawObjectToItemCorrespondence(curRawCorrespondence, ! mustSwapCorrespondences);
-
-                groupedListItems.get(posteSource).add(targetPoste);
-
             }
-
-            else {
-
-                // add targetItem
-                groupedListItems
-                    .get(posteSource)
-                    .add(mapRawObjectToItemCorrespondence(curRawCorrespondence, ! mustSwapCorrespondences));
-
-            }
+            // add targetItem
+            groupedListItems
+                .get(posteSource)
+                .add(mapRawObjectToItemCorrespondence(curRawCorrespondence, ! poste1IsSource));
         }
 
         return groupedListItems;
@@ -168,53 +117,34 @@ public class CorrespondencesUtils {
      * @param rawItemsList
      * @return
      */
-
-    private static Boolean shouldSwapRawCorespondences(
+    private static Boolean isPoste1Source(
         String codeClassificationSource,
         List<RawCorrespondence> rawItemsList) {
-
         // using the first correspondence ( rawItemsList.get(0) )
         // => which csv result, first or second uri field (uriPoste1/2) does contain classification source id ?
-
         String uriPoste1FromRaw = rawItemsList.get(0).getUriPoste1();
-
-        String toVerify = "/codes/" + codeClassificationSource + "/";
-
-        if ( ! uriPoste1FromRaw.contains(toVerify)) {
-
-            return true; // => last fields concern source classification
-        }
-
-        return false; // => first fields concern source classification;
-
+        String classifSource = "/codes/" + codeClassificationSource + "/";
+        return (uriPoste1FromRaw.contains(classifSource) ? false : true);
     }
 
-    private static Poste mapRawObjectToItemCorrespondence(RawCorrespondence corresp, boolean mustSwapAssociation) {
+    private static Poste mapRawObjectToItemCorrespondence(RawCorrespondence corresp, boolean poste1IsSource) {
+        return (poste1IsSource ? newPoste2(corresp) : newPoste1(corresp));
+    }
 
-        Poste item = null;
+    private static Poste newPoste1(RawCorrespondence corresp) {
+        return new Poste(
+            corresp.getCodePoste1(),
+            corresp.getUriPoste1(),
+            corresp.getIntituleFrPoste1(),
+            corresp.getIntituleEnPoste1());
+    }
 
-        if ( ! mustSwapAssociation) {
-
-            item =
-                new Poste(
-                    corresp.getCodePoste1(),
-                    corresp.getUriPoste1(),
-                    corresp.getIntituleFrPoste1(),
-                    corresp.getIntituleEnPoste1());
-
-        }
-        else {
-
-            item =
-                new Poste(
-                    corresp.getCodePoste2(),
-                    corresp.getUriPoste2(),
-                    corresp.getIntituleFrPoste2(),
-                    corresp.getIntituleEnPoste2());
-
-        }
-
-        return item;
+    private static Poste newPoste2(RawCorrespondence corresp) {
+        return new Poste(
+            corresp.getCodePoste2(),
+            corresp.getUriPoste2(),
+            corresp.getIntituleFrPoste2(),
+            corresp.getIntituleEnPoste2());
     }
 
 }
