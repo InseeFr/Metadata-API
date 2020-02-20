be84f25042eb60128be682fa796cc52b83a84baf
Alice Lambois <alice.lambois@insee.fr>
2020-02-20 14:23:34 +0100

    Update HealthcheckApi.java

--- src/main/java/fr/insee/rmes/api/HealthcheckApi.java
+++ src/main/java/fr/insee/rmes/api/HealthcheckApi.java
@@ -6,14 +6,24 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.lang3.StringUtils;
+
 @Path("/healthcheck")
 public class HealthcheckApi extends AbstractMetadataApi {
+    
+
+private final static String ASK_QUERY = "ASK { ?a a ?b .}";
 
     @GET
     @Produces({
         MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML
     })
     public Response getHealthcheck() {
+        if (StringUtils.isEmpty(sparqlUtils.executeSparqlQuery("SELECT * { ?s a ?t } LIMIT 1"))) {
+            return Response.serverError().build();
+        }
         return Response.ok().build();
     }
 }
+
+//https://qfrmessnczlht01.ad.insee.intra/sparql?query=ASK+{+?a+a+?b+.}
