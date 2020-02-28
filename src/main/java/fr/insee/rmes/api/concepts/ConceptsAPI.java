90d5637545b97be098ecc2735e8c8c2208d1d83a
Alice Lambois <alice.lambois@insee.fr>
2020-02-28 10:18:18 +0100

    Improve concept services by Code

--- src/main/java/fr/insee/rmes/api/concepts/ConceptsAPI.java
+++ src/main/java/fr/insee/rmes/api/concepts/ConceptsAPI.java
@@ -18,6 +18,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import fr.insee.rmes.api.AbstractMetadataApi;
+import fr.insee.rmes.modeles.concepts.Concept;
 import fr.insee.rmes.modeles.concepts.Definition;
 import fr.insee.rmes.modeles.concepts.Definitions;
 import fr.insee.rmes.queries.concepts.ConceptsQueries;
@@ -93,9 +94,9 @@ public class ConceptsAPI extends AbstractMetadataApi {
 
         logger.debug("Received GET request for Concept: {}", id);
 
-        Definition concept = new Definition(id);
+        Concept concept = new Concept(id);
         String csvResult = sparqlUtils.executeSparqlQuery(ConceptsQueries.getConceptById(id));
-        concept = (Definition) csvUtils.populatePOJO(csvResult, concept);
+        concept = (Concept) csvUtils.populatePOJO(csvResult, concept);
 
         if (concept.getUri() == null) {
             return Response.status(Status.NOT_FOUND).entity("").build();
--- src/main/java/fr/insee/rmes/queries/concepts/ConceptsQueries.java
+++ src/main/java/fr/insee/rmes/queries/concepts/ConceptsQueries.java
@@ -1,8 +1,16 @@
 package fr.insee.rmes.queries.concepts;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.commons.lang3.StringUtils;
 
-public class ConceptsQueries {
+import fr.insee.rmes.queries.Queries;
+
+public class ConceptsQueries extends Queries {
+    
+    private static final String QUERIES_FOLDER = "concepts/";
+
 
     public static String getConceptsByLabel(String label) {
 
@@ -22,18 +30,9 @@ public class ConceptsQueries {
     }
 
     public static String getConceptById(String id) {
-        return "SELECT ?uri ?intitule ?remplace ?estRemplacePar WHERE { \n"
-            + "?uri skos:inScheme ?conceptScheme . \n"
-            + "FILTER(REGEX(STR(?conceptScheme),'/concepts/definitions/scheme')) \n"
-            + "?uri skos:notation '"
-            + id
-            + "' . \n"
-            + "?uri skos:prefLabel ?intitule . \n"
-            + "FILTER(lang(?intitule) = 'fr') \n"
-            + "OPTIONAL{ ?uri dcterms:replaces ?remplace } \n"
-            + "OPTIONAL{ ?estRemplacePar dcterms:replaces ?uri } \n"
-            + "}"
-            + "ORDER BY ?intitule";
+        Map<String,Object> params = new HashMap<>();
+        params.put("id", id);
+        return buildRequest(QUERIES_FOLDER, "getConceptByCode.ftlh", params);
     }
 
 }
