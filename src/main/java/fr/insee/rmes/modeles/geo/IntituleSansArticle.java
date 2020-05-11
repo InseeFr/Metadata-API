6058d452f2062676cd7eea193d8e9f9474547d91
Alice Lambois <alice.lambois@insee.fr>
2020-05-11 14:36:14 +0200

    Sonar

--- src/main/java/fr/insee/rmes/modeles/geo/IntituleSansArticle.java
+++ src/main/java/fr/insee/rmes/modeles/geo/IntituleSansArticle.java
@@ -1,28 +1,32 @@
 package fr.insee.rmes.modeles.geo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlText;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description="Intitulé sans son article et article associé")
+@JsonPropertyOrder({"intituleSansArticle", "typeArticle"})
 public class IntituleSansArticle {
 
     @Schema(example = "Aigle")
-    private String intituleSansArticle = null;
+    private String label = null;
 
     @Schema(example = "5")
     private String typeArticle = null;
 
     
     @JacksonXmlText
+    @JsonProperty("intituleSansArticle")
+    @JacksonXmlProperty(localName="intituleSansArticle")
     public String getIntituleSansArticle() {
-        return intituleSansArticle;
+        return label;
     }
 
     public void setIntituleSansArticle(String intituleSansArticle) {
-        this.intituleSansArticle = intituleSansArticle;
+        this.label = intituleSansArticle;
     }
 
     @JacksonXmlProperty(isAttribute = true)
