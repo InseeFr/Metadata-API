b60c5ec3ad1549b11831bbe95f957abc0b38d3dc
Alice Lambois <alice.lambois@insee.fr>
2021-07-29 14:27:24 +0200

    Fix issue with href tag

--- src/main/java/fr/insee/rmes/utils/XmlUtils.java
+++ src/main/java/fr/insee/rmes/utils/XmlUtils.java
@@ -6,7 +6,8 @@ public class XmlUtils {
 		 return s.replaceAll("&lt;([a-zA-Z]+)&gt;", "<$1>") //xml open tag
 				 .replaceAll("&lt;/([a-zA-Z]+)&gt;", "</$1>") //xml close tag
 				 .replaceAll("&lt;([a-zA-Z]+)/&gt;", "<$1/>") //br
-				 .replaceAll("&lt;([a-zA-Z]+) /&gt;", "<$1 />"); //br with space
+				 .replaceAll("&lt;([a-zA-Z]+) /&gt;", "<$1 />") //br with space
+				 .replaceAll("&lt;a href=&quot;(.*?)&quot;&gt;", "<a href=\"$1\"> />"); //a href open tag
 	}
 
 }
