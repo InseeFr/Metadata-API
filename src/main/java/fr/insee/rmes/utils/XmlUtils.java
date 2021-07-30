ff5c086f82debf1018f7a87848d8cd8d1f2d6fe2
Alice Lambois <alice.lambois@insee.fr>
2021-07-30 09:22:29 +0200

    Add tag with style and improve linktag for xml encoder

--- src/main/java/fr/insee/rmes/utils/XmlUtils.java
+++ src/main/java/fr/insee/rmes/utils/XmlUtils.java
@@ -4,10 +4,14 @@ public class XmlUtils {
 	
 	public static String encodeXml(String s) {
 		 return s.replaceAll("&lt;([a-zA-Z]+)&gt;", "<$1>") //xml open tag
+				 .replaceAll("&lt;([a-zA-Z]+) style=(&quot;|')(.*?)(&quot;|')&gt;", "<$1 style=\"$3\">") //open tag with style
 				 .replaceAll("&lt;/([a-zA-Z]+)&gt;", "</$1>") //xml close tag
 				 .replaceAll("&lt;([a-zA-Z]+)/&gt;", "<$1/>") //br
 				 .replaceAll("&lt;([a-zA-Z]+) /&gt;", "<$1 />") //br with space
-				 .replaceAll("&lt;a href=&quot;(.*?)&quot;&gt;", "<a href=\"$1\"> />"); //a href open tag
+				 .replaceAll("&lt;a href=(&quot;|')(.*?)(&quot;|')&gt;", "<a href=\"$2\"> ") //a href open tag
+				 .replaceAll("&lt;a title=(&quot;|')(.*?)(&quot;|') href=(&quot;|')(.*?)(&quot;|')&gt;", "<a title=\"$2\" href=\"$5\"> ") //a title href open tag
+		 		.replaceAll("&lt;a href=(&quot;|')(.*?)(&quot;|') title=(&quot;|')(.*?)(&quot;|')&gt;", "<a title=\"$5\" href=\"$2\"> "); //a href title open tag
+
 	}
 
 }
