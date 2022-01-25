260e5fe7d2ff4e5a2230db219a6c2115f5d8f4ea
Alice Lambois <alice.lambois@insee.fr>
2022-01-25 14:46:57 +0100

    Add describe service

--- src/main/java/fr/insee/rmes/utils/SparqlUtils.java
+++ src/main/java/fr/insee/rmes/utils/SparqlUtils.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -19,18 +20,28 @@ public class SparqlUtils {
     private static Logger logger = LogManager.getLogger(SparqlUtils.class);
 
     public String executeSparqlQuery(String query) {
-
+        return executeSparqlQuery(query, "text/csv");
+    }
+    
+    public String executeSparqlQuery(String query, String resultType) {
         String uri = this.queryToURI(query);
         Client client = ClientBuilder.newBuilder().build();
-        String response = client.target(uri).request("text/csv").get(String.class);
-
+        String response = client.target(uri).request(resultType).get(String.class);
         logger.debug("SPARQL query returned: \n {}", response);
         return response;
     }
+    
+    
+    public boolean executeAskQuery(String query) {
+        String uri = Configuration.getSparqlEndPoint() + "?query=" + this.encode(query);
+        Client client = ClientBuilder.newBuilder().build();
+        String response = client.target(uri).request(MediaType.APPLICATION_XML).get(String.class);
+        logger.debug("SPARQL query returned: \n {}", response);
+        return response.contains("<boolean>true</boolean>");
+    }
 
-    public String queryToURI(String query) {
+    private String queryToURI(String query) {
         return Configuration.getSparqlEndPoint() + "?query=" + this.encode(QueryUtils.PREFIXES + query);
-
     }
 
     public String encode(String url) {
