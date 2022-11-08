cb4c6f875414f06c859ff3b46a483d3ca9eea8a8
Hugo Bouttes <hugo.bouttes@insee.fr>
2022-11-08 14:16:42 +0100

    changement des tests de ResponseUtils

--- src/test/java/fr/insee/rmes/utils/ResponseUtilsTest.java
+++ src/test/java/fr/insee/rmes/utils/ResponseUtilsTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import javax.ws.rs.core.MediaType;
 
-import fr.insee.rmes.modeles.operations.documentations.DocumentationSims;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -23,10 +22,8 @@ class ResponseUtilsTest {
     private ResponseUtils responseUtils;
     
     TestObject testObj;
-	DocumentationSims testSims;
     String expectedJson;
     String expectedXml;
-	String expectedXmlSims;
     
     @BeforeEach
     public void init() {
@@ -39,48 +36,38 @@ class ResponseUtilsTest {
     	initExpected(textToTry);
     	String resultXml = responseUtils.produceResponse(testObj, MediaType.APPLICATION_XML);
     	assertEquals(expectedXml,resultXml);
-
-		String resultXmlSims = responseUtils.produceResponse(testObj, MediaType.APPLICATION_XML);
-		assertEquals(expectedXmlSims,resultXmlSims);
-
+    	
     	String resultJson = responseUtils.produceResponse(testObj, MediaType.APPLICATION_JSON);
     	assertEquals(expectedJson,resultJson);
+    	
     }
 
     
     private void initExpected(String str) {
     	testObj = new TestObject();
     	testObj.setString(str);
-
-
-
+    	
     	RubriqueRichText r = new RubriqueRichText(str, Lang.FR);
     	testObj.setRubriqueRichText(r);
     	
-    	String strXmlSims = str.replace("&", "&amp;")
-				 .replace(">", "&gt;")
-				 .replace("<", "&lt;")
-				 .replace("\"", "&quot;")
-				 .replace("'", "&apos;");
-    	String strXml = str;
+    	//String strXml = str.replace("&", "&amp;")
+			//	 .replace(">", "&gt;")
+			//	 .replace("<", "&lt;")
+			//	 .replace("\"", "&quot;")
+			//	 .replace("'", "&apos;");
+    	
     	String strJson = str;
     	
     	
     	expectedXml =
     			"<TestObject>"
-    			+ "<string>"+strXml+"</string>"
-    			+ "<rubriqueRichText xml:lang=\"fr\"><Texte>" +strXml+"</Texte></rubriqueRichText>"
+    			+ "<string>"+str+"</string>"
+    			+ "<rubriqueRichText xml:lang=\"fr\"><Texte>" +str+"</Texte></rubriqueRichText>"
     			+ "</TestObject>";
     	
     	expectedJson =
     				"{\"string\":\""+strJson+"\","
     				+ "\"rubriqueRichText\":{\"texte\":\""+strJson+"\",\"langue\":\"fr\"}}";
-
-
-		expectedXmlSims = "<TestObject>"
-				+ "<string>"+strXmlSims+"</string>"
-				+ "<rubriqueRichText xml:lang=\"fr\"><Texte>" +strXmlSims+"</Texte></rubriqueRichText>"
-				+ "</TestObject>";
 	}
     
 
