c9a9c847ad20f06750d4a29e6f3d789bd95e4756
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2025-02-12 10:41:48 +0100

    fix (new interface implemented by GeoApi)

--- impl/src/main/java/fr/insee/rmes/metadata/api/GeoEndpoints.java
+++ impl/src/main/java/fr/insee/rmes/metadata/api/GeoEndpoints.java
@@ -1,8 +1,9 @@
 package fr.insee.rmes.metadata.api;
 
-import fr.insee.rmes.metadata.model.ListeDescendantsDepartementInner;
-import fr.insee.rmes.metadata.model.TypeEnumInclusDansDepartement;
 import fr.insee.rmes.metadata.requestprocessor.DescendantsRequestParametizer;
+import fr.insee.rmes.metadata.model.TypeEnumInclusDansDepartement;
+import fr.insee.rmes.metadata.model.ListeDescendantsDepartementInner;
+import fr.insee.rmes.metadata.api.GeoDepartementApi;
 import fr.insee.rmes.metadata.requestprocessor.RequestProcessorBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,11 +14,11 @@ import java.util.List;
 import static fr.insee.rmes.metadata.utils.EnpointsUtils.toResponseEntity;
 
 @Controller
-public class GeoEnpoints implements GeoApi {
+public class GeoEndpoints implements GeoDepartementApi {
 
     private final RequestProcessorBuilder requestProcessorBuilder;
 
-    public GeoEnpoints(RequestProcessorBuilder requestProcessorBuilder) {
+    public GeoEndpoints(RequestProcessorBuilder requestProcessorBuilder) {
         this.requestProcessorBuilder = requestProcessorBuilder;
     }
 
