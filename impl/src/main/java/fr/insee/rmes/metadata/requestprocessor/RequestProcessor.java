c8fbda4509d3f55632c01999ca1bef666d2d6019
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2025-02-14 16:26:47 +0100

    fix: typeOrigin for departement descendants

--- impl/src/main/java/fr/insee/rmes/metadata/requestprocessor/RequestProcessor.java
+++ impl/src/main/java/fr/insee/rmes/metadata/requestprocessor/RequestProcessor.java
@@ -11,7 +11,18 @@ import java.util.Map;
 public record RequestProcessor(QueryBuilder queryBuilder, Map<String, Object> parameters, Path queryFile, QueryExecutor queryExecutor, Unmarshaller unmarshaller) {
 
     public <E> List<E> listResult(Class<E> clazz) {
-        parameters.put("typeOrigine", clazz.getSimpleName());
+        String typeOrigine;
+        if (clazz.getSimpleName().startsWith("ListeDescendants")) {
+             typeOrigine = clazz.getSimpleName()
+                    .replace("ListeDescendants", "")
+                    .replace("Inner","");
+        } else {
+            typeOrigine = clazz.getSimpleName();
+        }
+
+        parameters.put("typeOrigine", typeOrigine);
+
+//        parameters.put("typeOrigine", clazz.getSimpleName());
         var query = queryBuilder.build(parameters, queryFile);
         return unmarshaller.unmarshalList(queryExecutor.execute(query), clazz);
     }
