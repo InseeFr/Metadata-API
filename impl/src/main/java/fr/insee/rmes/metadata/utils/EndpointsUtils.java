484174def43549276cb0f214068bbc58c49d4ef6
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2025-02-27 14:31:45 +0100

    fix: HttpMessageNotWritableException
    issue: No converter for [class java.util.ArrayList] with preset Content-Type 'null'

--- impl/src/main/java/fr/insee/rmes/metadata/utils/EndpointsUtils.java
+++ impl/src/main/java/fr/insee/rmes/metadata/utils/EndpointsUtils.java
@@ -1,7 +1,7 @@
 package fr.insee.rmes.metadata.utils;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-
 import java.util.List;
 
 public class EndpointsUtils {
@@ -9,7 +9,21 @@ public class EndpointsUtils {
     private EndpointsUtils() {}
 
     public static <E> ResponseEntity<List<E>> toResponseEntity(List<E> result) {
-        return result == null || result.isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(result);
+        if (result == null || result.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        MediaType contentType = MediaType.APPLICATION_JSON;
+        if (isXmlRequest()) {
+            contentType = MediaType.APPLICATION_XML;
+        }
+
+        return ResponseEntity.ok()
+                .contentType(contentType)
+                .body(result);
     }
 
+    private static boolean isXmlRequest() {
+        return false;
+    }
 }
