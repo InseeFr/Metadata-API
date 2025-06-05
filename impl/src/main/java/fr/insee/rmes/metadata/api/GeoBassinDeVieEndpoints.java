3f88bb509e53c621e5de6e146ea80fc69c8e929c
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2025-06-05 18:26:05 +0200

    feat: for geo/bassinDeVie2022/{code}

--- impl/src/main/java/fr/insee/rmes/metadata/api/GeoBassinDeVieEndpoints.java
+++ impl/src/main/java/fr/insee/rmes/metadata/api/GeoBassinDeVieEndpoints.java
@@ -1,30 +1,29 @@
 package fr.insee.rmes.metadata.api;
 
-
 import fr.insee.rmes.metadata.api.requestprocessor.RequestProcessor;
-import fr.insee.rmes.metadata.model.UniteUrbaine2020;
+import fr.insee.rmes.metadata.model.BassinDeVie2022;
 import fr.insee.rmes.metadata.queries.parameters.TerritoireRequestParametizer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import java.time.LocalDate;
 
+
 @Controller
-public class GeoUniteUrbaineEndpoints implements GeoUniteUrbaineApi {
+public class GeoBassinDeVieEndpoints implements GeoBassinDeVieApi {
 
     private final RequestProcessor requestProcessor;
 
-    public GeoUniteUrbaineEndpoints(RequestProcessor requestProcessor) {
+    public GeoBassinDeVieEndpoints(RequestProcessor requestProcessor) {
         this.requestProcessor = requestProcessor;
     }
 
     @Override
-    public ResponseEntity<UniteUrbaine2020> getcoguu(String code, LocalDate date) {
+    public ResponseEntity<BassinDeVie2022> getcogbass(String code, LocalDate date) {
         return requestProcessor.queryforFindTerritoire()
-                .with(new TerritoireRequestParametizer(code, date, UniteUrbaine2020.class, "none"))
+                .with(new TerritoireRequestParametizer(code, date, BassinDeVie2022.class, "none"))
                 .executeQuery()
-                .singleResult(UniteUrbaine2020.class).toResponseEntity();
+                .singleResult(BassinDeVie2022.class).toResponseEntity();
     }
 
 }
-
