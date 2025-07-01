504ae72eda38ecb135999a10140dbe98c9594bf7
Emmanuel <demey.emmanuel@gmail.com>
2025-07-01 11:11:50 +0200

    fix: remove unused import

--- impl/src/test/java/fr/insee/rmes/metadata/api/testcontainers/queries/GeoPaysQueriesTest.java
+++ impl/src/test/java/fr/insee/rmes/metadata/api/testcontainers/queries/GeoPaysQueriesTest.java
@@ -1,22 +1,12 @@
 package fr.insee.rmes.metadata.api.testcontainers.queries;
 
 import fr.insee.rmes.metadata.api.GeoDepartementEndpoints;
-import org.json.JSONArray;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.utility.DockerImageName;
 
 import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Tag("integration")
