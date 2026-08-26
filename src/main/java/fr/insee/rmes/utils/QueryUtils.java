94ea2d9692216a5f8d50af5f9d943790c2bf1b49
GtanSndil <mail@ad.insee.intra>
2026-08-26 15:25:02 +0200

    fix

--- src/main/java/fr/insee/rmes/utils/QueryUtils.java
+++ src/main/java/fr/insee/rmes/utils/QueryUtils.java
@@ -19,8 +19,8 @@ public class QueryUtils {
             + "PREFIX xsd:<http://www.w3.org/2001/XMLSchema#> \n"
             + "PREFIX prov:<http://www.w3.org/ns/prov#> \n"
             + "PREFIX foaf:<http://xmlns.com/foaf/0.1/> \n"
-            + "PREFIX schema:<http://schema.org> \n"
-            + "PREFIX dcmitype:<http://purl.org/dc/dcmitype> \n"
+            + "PREFIX schema:<http://schema.org/> \n"
+            + "PREFIX dcmitype:<http://purl.org/dc/dcmitype/> \n"
             + "PREFIX sdmx-mm:<http://www.w3.org/ns/sdmx-mm#> \n";
 
 }
