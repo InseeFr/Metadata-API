7860622587c8c70342eab7ca04f5d7f7101e2e62
GtanSndil <mail@ad.insee.intra>
2026-08-26 13:09:11 +0200

    style: in request

--- src/main/java/fr/insee/rmes/queries/operations/OperationsQueries.java
+++ src/main/java/fr/insee/rmes/queries/operations/OperationsQueries.java
@@ -99,6 +99,8 @@ public class OperationsQueries extends Queries {
 		params.put(ID_SIMS, idSims);
 		params.put("idRubric", idRubric);
 		params.put("LANG", lang.getUri());
+		params.put("LG1", Lang.FR);
+		params.put("LG2", Lang.EN);
 		return buildRequest(QUERIES_FOLDER, "getDocumentsQueryByIdSimsIdRubric.ftlh", params);
 	}
 
--- src/main/java/fr/insee/rmes/utils/QueryUtils.java
+++ src/main/java/fr/insee/rmes/utils/QueryUtils.java
@@ -18,6 +18,9 @@ public class QueryUtils {
             + "PREFIX pav:<http://purl.org/pav/> \n"
             + "PREFIX xsd:<http://www.w3.org/2001/XMLSchema#> \n"
             + "PREFIX prov:<http://www.w3.org/ns/prov#> \n"
+            + "PREFIX foaf:<http://xmlns.com/foaf/0.1/> \n"
+            + "PREFIX schema:<http://schema.org> \n"
+            + "PREFIX dcmitype:<http://purl.org/dc/dcmitype/Text> \n"
             + "PREFIX sdmx-mm:<http://www.w3.org/ns/sdmx-mm#> \n";
 
 }
